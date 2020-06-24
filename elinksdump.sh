7cb76aef0efd8e0f285bfc39e911b2e86fe88ac0
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-06-24 10:54:31 +0200

    support filename with whitespaces

--- elinksdump.sh
+++ elinksdump.sh
@@ -2,6 +2,6 @@
 
 file=$(echo $2 | sed 's/ /_/g')
 
-elinks -dump 1 $1 -dump-width 90 -no-references -no-numbering > $file
-echo $1 >> $file
+elinks -dump 1 "$1" -dump-width 90 -no-references -no-numbering > $file
+echo "$1" >> $file
 txt2otl.sh $file && rm -f $file
