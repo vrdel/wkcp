1438fa4710dde5fc8e592091c58f3324cafae199
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-04-05 11:21:52 +0200

    fallback to w3m when elinks fails

--- elinksdump.sh
+++ elinksdump.sh
@@ -3,5 +3,12 @@
 file=$(echo $2 | sed 's/ /_/g')
 
 elinks -dump 1 "$1" -dump-width 90 -no-references -no-numbering > $file
+
+let numlines=$(wc -l $file | awk '{print $1}')
+if ((numlines <= 2))
+then
+	w3m -dump $1 >| $file
+fi
+
 echo "$1" >> $file
 txt2otl.sh $file && rm -f $file
