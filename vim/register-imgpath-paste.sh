0b023e5be2d027e96e9af6d88f2a7f8e53b4a2b7
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-05-23 12:31:07 +0200

    added img placeholder for otl and md type

--- vim/register-imgpath-paste.sh
+++ vim/register-imgpath-paste.sh
@@ -2,10 +2,34 @@
 
 # vrdel
 
+while getopts 'mo' OPTION
+do
+	case $OPTION in
+		m) md=1 ;;
+		o) otl=1 ;;
+		h)
+			printf "Usage: %s
+              [-m generate md image placeholder]
+              [-o generate otl image placeholder]
+              file.md,otl\n" $(basename $0) >&2
+			exit 2
+			;;
+	esac
+done
+shift "$(( $OPTIND - 1 ))"
+
 file=$(xclip -o -selection clipboard)
 
-cp -f $file .
+if [ ! -z "$md" ]
+then
+	cp -f $file .
+	str="![$(basename $file)]($(basename $file))"
+	echo $str
+fi
 
-str="![$(basename $file)]($(basename $file))"
-
-echo $str
+if [ ! -z "$otl" ]
+then
+	cp -f $file .
+	str="myimg:$(basename $file)"
+	echo -e "\t: $str"
+fi
