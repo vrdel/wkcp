25ee472195c929be0b5e0e7dc05ba5f15952c3cb
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-04-22 15:08:45 +0200

    prepend only if relative path defined

--- wkcp/modules/image.py
+++ wkcp/modules/image.py
@@ -5,13 +5,22 @@ import copykitten
 MARKDOWN_IMAGE_PATTERN = re.compile(r'!\[.*?\]\((.*?)\)')
 WIKILINK_IMAGE_PATTERN = re.compile(r'!\[\[(.*?)\]\]')
 
+
+def _prepend_path(path_array):
+    temp_ip = list()
+    for path in path_array:
+        if not path.startswith('/'):
+            temp_ip.append(f'{os.getcwd()}/{path}')
+        else:
+            temp_ip.append(path)
+    return temp_ip
+
+
 def handle(args, stdin_content):
     image_path = MARKDOWN_IMAGE_PATTERN.findall(stdin_content)
     if image_path:
-        image_path = [f'{os.getcwd()}/{path}' for path in image_path]
-        print(image_path)
+        print(_prepend_path(image_path))
     else:
         image_path = WIKILINK_IMAGE_PATTERN.findall(stdin_content)
         if image_path:
-            image_path = [f'{os.getcwd()}/{path}' for path in image_path]
-            print(image_path)
+            print(_prepend_path(image_path))
