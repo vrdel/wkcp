2b24613e221292580a4a161c3583064c3c315d05
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-04-22 14:51:35 +0200

    prepend current path of image

--- wkcp/modules/image.py
+++ wkcp/modules/image.py
@@ -1,17 +1,17 @@
 import re
+import os
+import copykitten
 
 MARKDOWN_IMAGE_PATTERN = re.compile(r'!\[.*?\]\((.*?)\)')
 WIKILINK_IMAGE_PATTERN = re.compile(r'!\[\[(.*?)\]\]')
 
 def handle(args, stdin_content):
-    import ipdb; ipdb.set_trace()
-
     image_path = MARKDOWN_IMAGE_PATTERN.findall(stdin_content)
     if image_path:
-        for path in image_path:
-            print(path)
+        image_path = [f'{os.getcwd()}/{path}' for path in image_path]
+        print(image_path)
     else:
         image_path = WIKILINK_IMAGE_PATTERN.findall(stdin_content)
         if image_path:
-            for path in image_path:
-                print(path)
+            image_path = [f'{os.getcwd()}/{path}' for path in image_path]
+            print(image_path)
