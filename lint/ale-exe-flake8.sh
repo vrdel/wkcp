86bf7515b540596c4cbd75e284d03c753a845edb
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-06-16 11:49:05 +0200

    added linter settings

---
+++ lint/ale-exe-flake8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv activate linters-py39
+
+flake8 $@
