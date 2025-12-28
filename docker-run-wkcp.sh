b2b0d380d06ce4654d588c43304c24c89dde440e
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-12-28 11:53:18 +0100

    handle absolute path

--- docker-run-wkcp.sh
+++ docker-run-wkcp.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-docker run \
-	--net host \
-	--rm -v "$(pwd):/work" -w "/work" \
-	-e "DISPLAY=$DISPLAY" \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-t wkcp "$@"
+filemount=''
+
+for a in $*
+do
+	if $(test -f $a) && $(echo $a | egrep -q '^\/')
+	then
+		filemount="$a"
+	fi
+done
+
+if $(test ! -z $filemount)
+then
+	docker run \
+		--net host \
+		--rm -v "$(pwd):/work" -w "/work" \
+		-e "DISPLAY=$DISPLAY" \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v $filemount:$filemount:rw \
+		-t wkcp "$@"
+else
+	docker run \
+		--net host \
+		--rm -v "$(pwd):/work" -w "/work" \
+		-e "DISPLAY=$DISPLAY" \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-t wkcp "$@"
+fi
+
